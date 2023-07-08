99bfc564653504ff3a16a686f4f11350528d572e
Ruth <osukukuruth@gmail.com>
2023-07-07 22:27:25 -0400

    Done Assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
 
-
+class Book
+    attr_accessor :title, :author
+  
+    def initialize(title, author)
+      @title = title
+      @author = author
+    end
+  
+    def description
+      "#{title} by #{author}"
+    end
+  end
+  
